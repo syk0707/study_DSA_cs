d24d8a6958163438bb2893e5290f13f351f0ee4e
sykim <syk0707@naver.com>
2021-11-30 22:21:36 +0900

    add prog 12977

--- sort/Prog.cs
+++ sort/Prog.cs
@@ -2,19 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 
 namespace study_DSA_cs.sort
 {
     public class Prog
     {
+        public static IEnumerable<IEnumerable<T>> GetPermutations<T>(IEnumerable<T> list, int length)
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetPermutations(list, length - 1)
+                .SelectMany(t => list,
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetCombinationsRepeat<T>(IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetCombinationsRepeat(list, length - 1)
+                .SelectMany(t => list.Where(o => o.CompareTo(t.Last()) >= 0),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetCombinations<T>(IEnumerable<T> list, int length) where T : IComparable
+        {
+            if (length == 1) return list.Select(t => new T[] { t });
+            return GetCombinations(list, length - 1)
+                .SelectMany(t => list.Where(o => o.CompareTo(t.Last()) > 0),
+                    (t1, t2) => t1.Concat(new T[] { t2 }));
+        }
+
         public static void _42746()
         {
-            int[] numbers = new int[5];
-            numbers[0] = 3;
-            numbers[1] = 30;
-            numbers[2] = 34;
-            numbers[3] = 5;
-            numbers[4] = 9;
+            int[] numbers = new int[5] { 3, 30, 34, 5, 9};
             string answer = "";
             int maxLen = 1000;
             StringBuilder sb = new StringBuilder();
@@ -44,6 +64,32 @@ namespace study_DSA_cs.sort
                 sb.Append("0");
             }
             answer = sb.ToString();
+        }
+
+        public static void _12977()
+        {
+            int[] nums = new int[5] { 1, 2, 7, 6, 4 };
+            //var sortNum = nums.OrderBy(num => num).ToList();
+            //int maxNum = sortNum[nums.Length - 1];
+            var caseList = GetCombinations<int>(nums, 3);
+            //Console.WriteLine(JsonSerializer.Serialize(sortNum));
+            int answer = 0;
+            foreach(var caseItem in caseList)
+            {
+                int calNum = caseItem.Sum();
+                bool isDivideNum = false;
+                for (var divideNum = 2; divideNum < calNum; divideNum++)
+                {
+                    if (calNum % divideNum == 0)
+                    {
+                        isDivideNum = true;
+                        break;
+                    }
+                }
+                if (!isDivideNum)
+                    answer++;
+            }
+            Console.WriteLine(answer);
         }
     }
 }
